4e38aed9c1388e9dc86aae77d04074eea1d9567d
Pavan Kumar <npavankumar918@gmail.com>
2022-03-17 21:19:53 +0530

    first commit

---
+++ src/main/java/com/pavan/spring/paypal/api/Order.java
@@ -0,0 +1,25 @@
+package com.pavan.spring.paypal.api;
+
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+
+public class Order {
+
+	private double price;
+	private String currency;
+	private String method;
+	private String intent;
+	private String description;
+
+	
+	
+}
